3f31a0f41c3836fa8c14e57193a8964fa7deb775
victorinchrist <yeohun3333@gmail.com>
2024-10-04 14:56:33 -0400

    first commit

---
+++ jacket.h
@@ -0,0 +1,23 @@
+/*
+ * Exported with nin10kit v1.8
+ * Invocation command was nin10kit --mode=3 --resize=40x40 jacket jacket.png 
+ * Time-stamp: Friday 03/29/2024, 19:19:47
+ * 
+ * Image Information
+ * -----------------
+ * jacket.png 40@40
+ * 
+ * All bug reports / feature requests are to be filed here https://github.com/TricksterGuy/nin10kit/issues
+ */
+
+#ifndef JACKET_H
+#define JACKET_H
+
+extern const unsigned short jacket[1600];
+#define JACKET_SIZE 3200
+#define JACKET_LENGTH 1600
+#define JACKET_WIDTH 40
+#define JACKET_HEIGHT 40
+
+#endif
+
